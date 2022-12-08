aneax/Enzyme

Add forward mode benchmarks

enzyme/benchmarks/ForwardMode/ode-const/ode-adept.cpp
@@ -1,0 +1,100 @@
+#include <adept.h>
+#include <adept_source.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+using adept::adouble;
+
+static float tdiff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+#define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
+#define BOOST_NO_EXCEPTIONS
+#include <boost/array.hpp>
+#include <iostream>
+
+#include <boost/numeric/odeint.hpp>
+
+#include <boost/throw_exception.hpp>
+
+using namespace std;
+using namespace boost::numeric::odeint;
+
+double foobar(double t, uint64_t iters);
+
+typedef boost::array<adouble, 1> astate_type;
+
+void alorenz(const astate_type &x, astate_type &dxdt, adouble t) {
+  const double a = 1.2;
+  dxdt[0] = -a * x[0];
+}
+
+adouble afoobar(adouble t, uint64_t iters) {
+  astate_type x = {1.0}; // initial conditions
+
+  adouble start = 0.0;
+  adouble step = t / adouble(iters);
+  // typedef controlled_runge_kutta< runge_kutta_dopri5< astate_type , typename
+  // astate_type::value_type , astate_type , adouble > > stepper_type;
+  typedef euler<astate_type, typename astate_type::value_type, astate_type,
+                adouble>
+      stepper_type;
+  integrate_const(stepper_type(), alorenz, x, start, t, step);
+
+  // x[0] += -1.2 * step * x[0];
+
+  // printf("final result t=%f x(t)=%f, exp(-1.2* t)=%f\n", t, x[0], exp(- 1.2 *
+  // t));
+  return x[0];
+}
+
+static double afoobar_and_gradient(double xin, double &xgrad, uint64_t iters) {
+  adept::Stack stack;
+  adouble x = xin;
+  stack.new_recording();
+  adouble y = afoobar(x, iters);
+  y.set_gradient(1.0);
+  stack.compute_adjoint();
+  xgrad = x.get_gradient();
+  return y.value();
+}
+
+void adept_sincos(double inp, uint64_t iters) {
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = foobar(inp, iters);
+
+    gettimeofday(&end, NULL);
+    printf("Adept real %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    adept::Stack stack;
+    // stack.new_recording();
+    adouble resa = afoobar(inp, iters);
+    double res = resa.value();
+
+    gettimeofday(&end, NULL);
+    printf("Adept forward %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res2 = 0;
+    afoobar_and_gradient(inp, res2, iters);
+
+    gettimeofday(&end, NULL);
+    printf("Adept combined %0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}

enzyme/benchmarks/ForwardMode/taylorlog/taylorlog.cpp
@@ -1,0 +1,169 @@
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+template <typename Return, typename... T> Return __enzyme_autodiff(T...);
+
+float tdiff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+#include <adept.h>
+#include <adept_source.h>
+using adept::adouble;
+
+#define SINCOSN 10000000
+static double sincos_real(double x) {
+  double sum = 0;
+  for (int i = 1; i <= SINCOSN; i++) {
+    sum += pow(x, i) / i;
+  }
+  return sum;
+}
+
+static void sincos_real_tapenade(double x, double *xb, double sincos_realb) {
+  double sum = 0;
+  double sumb = 0.0;
+  double sincos_real;
+  sumb = sincos_realb;
+  for (int i = SINCOSN; i > 0; --i)
+    if (!(x <= 0.0 && (i == 0.0 || i != (int)i)))
+      *xb = *xb + pow(x, (i - 1)) * sumb;
+}
+
+static adouble sincos(adouble x) {
+  adouble sum = 0;
+  for (int i = 1; i <= SINCOSN; i++) {
+    sum += pow(x, i) / i;
+  }
+  return sum;
+}
+
+static double sincos_and_gradient(double xin, double &xgrad) {
+  adept::Stack stack;
+  adouble x = xin;
+  stack.new_recording();
+  adouble y = sincos(x);
+  y.set_gradient(1.0);
+  stack.compute_adjoint();
+  xgrad = x.get_gradient();
+  return y.value();
+}
+
+static void adept_sincos(double inp) {
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    adept::Stack stack;
+    // stack.new_recording();
+    adouble resa = sincos(inp);
+    double res = resa.value();
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res2 = 0;
+    sincos_and_gradient(inp, res2);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+static void tapenade_sincos(double inp) {
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("tapenade %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("tapenade %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    double res2 = 0;
+
+    sincos_real_tapenade(inp, &res2, 1.0);
+
+    gettimeofday(&end, NULL);
+    printf("tapendade %0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+static void enzyme_sincos(double inp) {
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    double res2;
+
+    res2 = __enzyme_autodiff<double>(sincos_real, inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+int main(int argc, char **argv) {
+
+  double inp = atof(argv[1]);
+  printf("adept\n");
+  adept_sincos(inp);
+  printf("tapenade\n");
+  tapenade_sincos(inp);
+  printf("enzyme\n");
+  enzyme_sincos(inp);
+}

EnzymeAD/Enzyme-MLIR-Reverse

enzyme/benchmarks/ForwardMode/ode-const/ode-adept.cpp
@@ -1,0 +1,100 @@
+#include <adept.h>
+#include <adept_source.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+using adept::adouble;
+
+static float tdiff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+#define BOOST_MATH_NO_LONG_DOUBLE_MATH_FUNCTIONS
+#define BOOST_NO_EXCEPTIONS
+#include <boost/array.hpp>
+#include <iostream>
+
+#include <boost/numeric/odeint.hpp>
+
+#include <boost/throw_exception.hpp>
+
+using namespace std;
+using namespace boost::numeric::odeint;
+
+double foobar(double t, uint64_t iters);
+
+typedef boost::array<adouble, 1> astate_type;
+
+void alorenz(const astate_type &x, astate_type &dxdt, adouble t) {
+  const double a = 1.2;
+  dxdt[0] = -a * x[0];
+}
+
+adouble afoobar(adouble t, uint64_t iters) {
+  astate_type x = {1.0}; // initial conditions
+
+  adouble start = 0.0;
+  adouble step = t / adouble(iters);
+  // typedef controlled_runge_kutta< runge_kutta_dopri5< astate_type , typename
+  // astate_type::value_type , astate_type , adouble > > stepper_type;
+  typedef euler<astate_type, typename astate_type::value_type, astate_type,
+                adouble>
+      stepper_type;
+  integrate_const(stepper_type(), alorenz, x, start, t, step);
+
+  // x[0] += -1.2 * step * x[0];
+
+  // printf("final result t=%f x(t)=%f, exp(-1.2* t)=%f\n", t, x[0], exp(- 1.2 *
+  // t));
+  return x[0];
+}
+
+static double afoobar_and_gradient(double xin, double &xgrad, uint64_t iters) {
+  adept::Stack stack;
+  adouble x = xin;
+  stack.new_recording();
+  adouble y = afoobar(x, iters);
+  x.set_gradient(1.0);
+  stack.compute_tangent_linear();
+  xgrad = y.get_gradient();
+  return y.value();
+}
+
+void adept_sincos(double inp, uint64_t iters) {
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = foobar(inp, iters);
+
+    gettimeofday(&end, NULL);
+    printf("Adept real %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    adept::Stack stack;
+    // stack.new_recording();
+    adouble resa = afoobar(inp, iters);
+    double res = resa.value();
+
+    gettimeofday(&end, NULL);
+    printf("Adept forward %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res2 = 0;
+    afoobar_and_gradient(inp, res2, iters);
+
+    gettimeofday(&end, NULL);
+    printf("Adept combined %0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}

enzyme/benchmarks/ForwardMode/taylorlog/taylorlog.cpp
@@ -1,0 +1,169 @@
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+template <typename Return, typename... T> Return __enzyme_fwddiff(T...);
+
+float tdiff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+#include <adept.h>
+#include <adept_source.h>
+using adept::adouble;
+
+#define SINCOSN 10000000
+static double sincos_real(double x) {
+  double sum = 0;
+  for (int i = 1; i <= SINCOSN; i++) {
+    sum += pow(x, i) / i;
+  }
+  return sum;
+}
+
+static void sincos_real_tapenade(double x, double *xb, double sincos_realb) {
+  double sum = 0;
+  double sumb = 0.0;
+  double sincos_real;
+  sumb = sincos_realb;
+  for (int i = SINCOSN; i > 0; --i)
+    if (!(x <= 0.0 && (i == 0.0 || i != (int)i)))
+      *xb = *xb + pow(x, (i - 1)) * sumb;
+}
+
+static adouble sincos(adouble x) {
+  adouble sum = 0;
+  for (int i = 1; i <= SINCOSN; i++) {
+    sum += pow(x, i) / i;
+  }
+  return sum;
+}
+
+static double sincos_and_gradient(double xin, double &xgrad) {
+  adept::Stack stack;
+  adouble x = xin;
+  stack.new_recording();
+  adouble y = sincos(x);
+  x.set_gradient(1.0);
+  stack.compute_tangent_linear();
+  xgrad = y.get_gradient();
+  return y.value();
+}
+
+static void adept_sincos(double inp) {
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    adept::Stack stack;
+    // stack.new_recording();
+    adouble resa = sincos(inp);
+    double res = resa.value();
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res2 = 0;
+    sincos_and_gradient(inp, res2);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+static void tapenade_sincos(double inp) {
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("tapenade %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("tapenade %0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    double res2 = 0;
+
+    sincos_real_tapenade(inp, &res2, 1.0);
+
+    gettimeofday(&end, NULL);
+    printf("tapendade %0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+static void enzyme_sincos(double inp) {
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    double res = sincos_real(inp);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res=%f\n", tdiff(&start, &end), res);
+  }
+
+  {
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    double res2;
+
+    res2 = __enzyme_fwddiff<double>(sincos_real, inp, 1.0);
+
+    gettimeofday(&end, NULL);
+    printf("%0.6f res'=%f\n", tdiff(&start, &end), res2);
+  }
+}
+
+int main(int argc, char **argv) {
+
+  double inp = atof(argv[1]);
+  printf("adept\n");
+  adept_sincos(inp);
+  printf("tapenade\n");
+  tapenade_sincos(inp);
+  printf("enzyme\n");
+  enzyme_sincos(inp);
+}