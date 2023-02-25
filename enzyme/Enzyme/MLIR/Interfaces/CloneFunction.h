a50bcec8e1e612963b01f6fd2314f5ce94f387ce
martin eppert <martin.eppert@web.de>
2023-02-25 03:57:10 +0000

    removed using namespace

--- enzyme/Enzyme/MLIR/Interfaces/CloneFunction.h
+++ enzyme/Enzyme/MLIR/Interfaces/CloneFunction.h
@@ -3,35 +3,37 @@
 #include "EnzymeLogic.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 
-using namespace mlir;
-using namespace mlir::enzyme;
+mlir::Type getShadowType(mlir::Type type, unsigned width = 1);
 
-Type getShadowType(Type type, unsigned width = 1);
+mlir::FunctionType getFunctionTypeForClone(
+    mlir::FunctionType FTy, mlir::enzyme::DerivativeModeMLIR mode,
+    unsigned width, mlir::Type additionalArg,
+    llvm::ArrayRef<mlir::enzyme::DIFFE_TYPE_MLIR> constant_args,
+    bool diffeReturnArg, mlir::enzyme::ReturnTypeMLIR returnValue,
+    mlir::enzyme::DIFFE_TYPE_MLIR ReturnTypeMLIR);
 
-mlir::FunctionType
-getFunctionTypeForClone(mlir::FunctionType FTy, DerivativeModeMLIR mode,
-                        unsigned width, mlir::Type additionalArg,
-                        llvm::ArrayRef<DIFFE_TYPE_MLIR> constant_args,
-                        bool diffeReturnArg, ReturnTypeMLIR returnValue,
-                        DIFFE_TYPE_MLIR ReturnTypeMLIR);
-
-void cloneInto(Region *src, Region *dest, Region::iterator destPos,
-               BlockAndValueMapping &mapper,
-               std::map<Operation *, Operation *> &opMap);
-
-void cloneInto(Region *src, Region *dest, BlockAndValueMapping &mapper,
+void cloneInto(mlir::Region *src, mlir::Region *dest,
+               mlir::Region::iterator destPos,
+               mlir::BlockAndValueMapping &mapper,
                std::map<mlir::Operation *, mlir::Operation *> &opMap);
 
-Operation *clone(Operation *src, BlockAndValueMapping &mapper,
-                 Operation::CloneOptions options,
-                 std::map<Operation *, Operation *> &opMap);
+void cloneInto(mlir::Region *src, mlir::Region *dest,
+               mlir::BlockAndValueMapping &mapper,
+               std::map<mlir::Operation *, mlir::Operation *> &opMap);
 
-FunctionOpInterface CloneFunctionWithReturns(
-    DerivativeModeMLIR mode, unsigned width, FunctionOpInterface F,
-    BlockAndValueMapping &ptrInputs, ArrayRef<DIFFE_TYPE_MLIR> constant_args,
-    SmallPtrSetImpl<mlir::Value> &constants,
-    SmallPtrSetImpl<mlir::Value> &nonconstants,
-    SmallPtrSetImpl<mlir::Value> &returnvals, ReturnTypeMLIR returnValue,
-    DIFFE_TYPE_MLIR ReturnTypeMLIR, Twine name, BlockAndValueMapping &VMap,
-    std::map<Operation *, Operation *> &OpMap, bool diffeReturnArg,
+mlir::Operation *clone(mlir::Operation *src, mlir::BlockAndValueMapping &mapper,
+                       mlir::Operation::CloneOptions options,
+                       std::map<mlir::Operation *, mlir::Operation *> &opMap);
+
+mlir::FunctionOpInterface CloneFunctionWithReturns(
+    mlir::enzyme::DerivativeModeMLIR mode, unsigned width,
+    mlir::FunctionOpInterface F, mlir::BlockAndValueMapping &ptrInputs,
+    mlir::ArrayRef<mlir::enzyme::DIFFE_TYPE_MLIR> constant_args,
+    mlir::SmallPtrSetImpl<mlir::Value> &constants,
+    mlir::SmallPtrSetImpl<mlir::Value> &nonconstants,
+    mlir::SmallPtrSetImpl<mlir::Value> &returnvals,
+    mlir::enzyme::ReturnTypeMLIR returnValue,
+    mlir::enzyme::DIFFE_TYPE_MLIR ReturnTypeMLIR, mlir::Twine name,
+    mlir::BlockAndValueMapping &VMap,
+    std::map<mlir::Operation *, mlir::Operation *> &OpMap, bool diffeReturnArg,
     mlir::Type additionalArg);
